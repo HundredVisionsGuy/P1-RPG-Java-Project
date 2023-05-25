bec8c3b5422352629cec2b220fac49f22f87967e
hundredvisionsguy <winikkc@hsd.k12.or.us>
2023-05-24 17:20:03 -0700

    Adding Character class with getters and setters

--- src/main/java/com/rpg_example/App.java
+++ src/main/java/com/rpg_example/App.java
@@ -12,10 +12,16 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        // Create a character object
-        Character hero = new Character("Humar");
-        System.out.println("We have a new character!");
-        System.out.println(hero.getName());
+        System.out.println("We have a new character!\n");
+        Character hero = new Character("Phred");
+        System.out.println(hero);
+
+        // Demonstrate the getters and setters
+        hero.setDexterity(9);
+        hero.setStrength(11);
+        hero.setStrength(-5);
+        System.out.println(hero.getDexterity());
+
         System.out.println(hero);
     }
 }
--- src/main/java/com/rpg_example/Character.java
+++ src/main/java/com/rpg_example/Character.java
@@ -1,17 +1,17 @@
 package com.rpg_example;
 
 public class Character {
-    // private fields
+    // add fields
     private String name;
     private int strength;
     private int dexterity;
 
-    // instance methods
+    // Constructor Method
     public Character(String name) {
         this.name = name;
     }
 
-    // getters and setters
+    // Getters and Setters
     public String getName() {
         return name;
     }
@@ -21,23 +21,23 @@ public class Character {
     }
 
     public void setStrength(int strength) {
-        this.strength = strength;
+        this.strength += strength;
     }
-    
+
     public int getDexterity() {
         return dexterity;
     }
 
     public void setDexterity(int dexterity) {
-        this.dexterity = dexterity;
+        this.dexterity += dexterity;
     }
-
 
     // toString override
     public String toString() {
-        String output = "Character Name: " + name;
-        output += "\nStrength: " + strength;
-        output += "\nDexterity: " + dexterity;
+        String output = "Character\n";
+        output += "Name: " + this.name;
+        output += "\nStrength: " + this.strength;
+        output += "\nDexterity: " + this.dexterity;
         return output;
     }
 }
